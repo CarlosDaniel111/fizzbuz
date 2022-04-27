df7876826d2f71ee58a82a27899173f9768034b2
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2022-04-26 21:19:05 -0600

    Fix tests

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,7 +1,7 @@
-const Reader = require('./../../lib/utils/Reader')
+const Reader = require('../../lib/utils/Reader')
 
 describe('Unit Test for Reader', () => {
-    test('1) Checking for correct Json read', () => {
+    test('1. Checking for correct Json read', () => {
         const explorers = Reader.readJsonFile('explorers.json')
         expect(explorers[0].name).toBe("Woopa1")
     });
