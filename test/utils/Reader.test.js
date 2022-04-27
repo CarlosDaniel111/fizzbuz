176e274cc6a9b469e1686c44a1c6ff4127cc12e7
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2022-04-26 20:51:03 -0600

    Add test Reader.js

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require('./../../lib/utils/Reader')
+
+describe('Unit Test for Reader', () => {
+    test('1) Checking for correct Json read', () => {
+        const explorers = Reader.readJsonFile('explorers.json')
+        expect(explorers[0].name).toBe("Woopa1")
+    });
+  })
