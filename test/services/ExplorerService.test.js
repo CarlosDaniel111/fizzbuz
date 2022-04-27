bfe4069bf92252929a973cc88a0aea99ed8d6add
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2022-04-26 21:24:13 -0600

    Fix tests

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,5 +1,5 @@
-const ExplorerService = require('../../lib/services/ExplorerService')
-const Reader = require('../../lib/utils/Reader')
+const ExplorerService = require("../../lib/services/ExplorerService")
+const Reader = require("../../lib/utils/Reader")
 
 describe('Unit Test for ExplorerService', () => {
     test('1. Filter explorer objects by mission', () => {
