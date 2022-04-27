41223d440e0120629659d03a176fcc395256ea4a
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2022-04-26 21:42:20 -0600

    Add linter and fix project

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,20 +1,20 @@
 const ExplorerService = require("../../lib/services/ExplorerService");
 const Reader = require("../../lib/utils/Reader");
 
-describe('Unit Test for ExplorerService', () => {
-    test('1. Filter explorer objects by mission', () => {
-        const explorers = Reader.readJsonFile('explorers.json')
+describe("Unit Test for ExplorerService", () => {
+    test("1. Filter explorer objects by mission", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
         const noders = ExplorerService.filterByMission(explorers, "node");
-        expect(noders[0].mission).toBe('node');
+        expect(noders[0].mission).toBe("node");
     });
-    test('2. Amount of explorers by mission', () => {
-        const explorers = Reader.readJsonFile('explorers.json')
-        const amountExplorersNode = ExplorerService.getAmountOfExplorersByMission(explorers, 'node')
-        expect(amountExplorersNode).toBe(10)
+    test("2. Amount of explorers by mission", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const amountExplorersNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(amountExplorersNode).toBe(10);
     });
-    test('3. Usernames by mission', () => {
-        const explorers = Reader.readJsonFile('explorers.json')
-        const nodersUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, 'node')
-        expect(nodersUsernames[0]).toBe('ajolonauta1')
-    })
-})
+    test("3. Usernames by mission", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const nodersUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(nodersUsernames[0]).toBe("ajolonauta1");
+    });
+});
